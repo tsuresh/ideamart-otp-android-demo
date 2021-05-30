bf435938a7ad583d7a3b8e4f15c3eca67af8f291
Suresh Peiris <tsmpmail@gmail.com>
2021-05-30 19:07:35 +0530

    fixes some issues

--- android/app/src/main/java/com/inforwaves/ideamartotp/data/repository/LoginUtil.java
+++ android/app/src/main/java/com/inforwaves/ideamartotp/data/repository/LoginUtil.java
@@ -21,6 +21,7 @@ import okhttp3.Response;
 public class LoginUtil {
 
     private Context context;
+    private static final String BACKEND = "https://sv2.ideamarthosting.dialog.lk/sureshp72540920Apps/demo/";
 
     public LoginUtil(Context context) {
         this.context = context;
@@ -32,9 +33,9 @@ public class LoginUtil {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n    \"subscriberId\" : \""+mobileNumber+"\",\r\n    \"applicationHash\" : \"1234dsadsadsa\",\r\n    \"device\" : \"Pixel\",\r\n    \"os\" : \"Android\",\r\n    \"appCode\" : \"10\"\r\n}");
+        RequestBody body = RequestBody.create(mediaType, "{\r\n    \"subscriberId\" : \"tel:94"+mobileNumber+"\",\r\n    \"applicationHash\" : \"1234dsadsadsa\",\r\n    \"device\" : \"Pixel\",\r\n    \"os\" : \"Android\",\r\n    \"appCode\" : \"10\"\r\n}");
         Request request = new Request.Builder()
-                .url("https://sv2.ideamarthosting.dialog.lk/sureshp72540920Apps/demo/requestOtp.php")
+                .url(BACKEND + "requestOtp.php")
                 .method("POST", body)
                 .addHeader("Content-Type", "application/json")
                 .build();
@@ -46,7 +47,7 @@ public class LoginUtil {
                 String statusDetail = object.getString("statusDetail");
                 String statusCode = object.getString("statusCode");
                 if(statusCode.equals("S1000")){
-                    String referenceNumber = object.getString("referenceNumber");
+                    String referenceNumber = object.getString("referenceNo");
                     callback.onSuccess(new Message(
                             statusDetail,
                             statusCode,
@@ -78,7 +79,7 @@ public class LoginUtil {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, "{\r\n    \"referenceNo\" : \""+refNumber+"\",\r\n    \""+otpCode+"\" : \"1234\"\r\n}");
         Request request = new Request.Builder()
-                .url("https://sv2.ideamarthosting.dialog.lk/sureshp72540920Apps/demo/verifyOtp.php")
+                .url(BACKEND + "verifyOtp.php")
                 .method("POST", body)
                 .addHeader("Content-Type", "application/json")
                 .build();
